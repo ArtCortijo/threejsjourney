6fb8255de37e39ab80d7acf0b6d7d23efb8a534f
Arturo Cortijo Purizaca <acpurizaca@experience-yhp.com>
2022-04-05 15:53:00 -0400

    08-Resizing & Fullscreen

--- src/script.js
+++ src/script.js
@@ -1,22 +1,6 @@
 import './style.css';
 import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
-
-/**
- * Cursor
- */
-
-const cursor = {
-	x: 0,
-	y: 0,
-};
-// Cursor coordination
-window.addEventListener('mousemove', (e) => {
-	cursor.x = e.clientX / sizes.width - 0.5; // the - 0.5 is so it goes from -0.5 to 0.5, so we have a from left to right
-	cursor.y = -(e.clientY / sizes.height - 0.5);
-
-	console.log(cursor.x);
-});
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 /**
  * Base
@@ -24,61 +8,103 @@ window.addEventListener('mousemove', (e) => {
 // Canvas
 const canvas = document.querySelector('canvas.webgl');
 
-// Sizes
-const sizes = {
-	width: 800,
-	height: 600,
-};
-
 // Scene
 const scene = new THREE.Scene();
 
-// Object
-const mesh = new THREE.Mesh(
-	new THREE.BoxGeometry(1, 1, 1, 5, 5, 5),
-	new THREE.MeshBasicMaterial({ color: 0xff0000 })
-);
+/**
+ * Object
+ */
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
 
-// Camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
-// camera.position.x = 2;
-// camera.position.y = 2;
-camera.position.z = 2;
-camera.lookAt(mesh.position);
+/**
+ * Sizes
+ */
+const sizes = {
+	width: window.innerWidth,
+	height: window.innerHeight,
+};
+
+/**
+ * Resize
+ */
+window.addEventListener('resize', () => {
+	//Update sizes
+	sizes.width = window.innerWidth;
+	sizes.height = window.innerHeight;
+
+	//Update camera
+	camera.aspect = sizes.width / sizes.height;
+	camera.updateProjectionMatrix();
+
+	//Update renderer
+	renderer.setSize(sizes.width, sizes.height);
+	renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
+
+/**
+ * Going fullscreen
+ */
+window.addEventListener('dblclick', () => {
+	// This works everywhere except Safari
+	// !document.fullscreenElement
+	// 	? canvas.requestFullscreen()
+	// 	: document.exitFullscreen();
+
+	//Here's the prefix version
+	const fullscreenElement =
+		document.fullscreenElement || document.webkitFullscreenElement;
+	if (!fullscreenElement) {
+		if (canvas.requestFullscreen) {
+			canvas.requestFullscreen();
+		} else if (canvas.webkitRequestFullscreen) {
+			canvas.webkitRequestFullscreen();
+		}
+	} else {
+		if (document.exitFullscreen) {
+			document.exitFullscreen();
+		} else if (document.webkitExitFullscreen) {
+			document.webkitExitFullscreen();
+		}
+	}
+});
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(
+	75,
+	sizes.width / sizes.height,
+	0.1,
+	100
+);
+camera.position.z = 3;
 scene.add(camera);
 
-// Controls - Orbit
+// Controls
 const controls = new OrbitControls(camera, canvas);
 controls.enableDamping = true;
-// You aim slightly above the camera like so:
-// controls.target.y = 1;
-// controls.update();
 
-// Renderer
+/**
+ * Renderer
+ */
 const renderer = new THREE.WebGLRenderer({
 	canvas: canvas,
 });
 renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(window.devicePixelRatio);
+// if you have a devicePixelRatio above 2, like 5 : renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
-// Animate
+/**
+ * Animate
+ */
 const clock = new THREE.Clock();
 
 const tick = () => {
 	const elapsedTime = clock.getElapsedTime();
-
-	// Update objects
-	// mesh.rotation.y = elapsedTime;
-
-	// Update camera
-	// camera.position.x = cursor.x * 3;
-	// camera.position.y = cursor.y * 3;
-
-	// To rotate around the square
-	// camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3;
-	// camera.position.y = cursor.y * 5;
-	// camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3;
-	// camera.lookAt(mesh.position); // it's the same as : camera.lookAt(new THREE.Vector3(0, 0, 0));
 
 	// Update controls
 	controls.update();
