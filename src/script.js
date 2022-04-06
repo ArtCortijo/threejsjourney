243d644277c3146c1de63ba9937adbe64cfd82c1
Arturo Cortijo Purizaca <acpurizaca@experience-yhp.com>
2022-04-06 15:05:37 -0400

    10-Debig UI

--- src/script.js
+++ src/script.js
@@ -1,6 +1,24 @@
 import './style.css';
 import * as THREE from 'three';
+import * as dat from 'dat.gui';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import gsap from 'gsap';
+
+/**
+ * Debug
+ */
+const gui = new dat.GUI({ closed: true, width: 400 });
+
+const parameters = {
+	color: 0xff0000,
+	spin: () => {
+		console.log('spin');
+		gsap.to(mesh.rotation, {
+			duration: 1,
+			y: mesh.rotation.y + Math.PI * 2,
+		});
+	},
+};
 
 /**
  * Base
@@ -11,28 +29,31 @@ const canvas = document.querySelector('canvas.webgl');
 // Scene
 const scene = new THREE.Scene();
 
-// Object
-// const geometry = new THREE.BoxGeometry(1, 1, 1);
-const geometry = new THREE.BufferGeometry();
-const count = 50; // 50 triangles
-const positionsArray = new Float32Array(count * 3 * 3); // each triangle will be composed of 3 vertices and each vertex will be composed of 3 values
-
-for (let i = 0; i < count * 3 * 3; i++) {
-	positionsArray[i] = Math.random() - 0.5; // the - 0,5 is to center it
-}
-
-const positionsAttribute = new THREE.BufferAttribute(positionsArray, 3);
-
-geometry.setAttribute('position', positionsAttribute);
-
-const material = new THREE.MeshBasicMaterial({
-	color: 0xff0000,
-	wireframe: true,
-});
+/**
+ * Object
+ */
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: parameters.color });
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
 
-// Sizes
+// Debug
+// gui allows to modify an object's property
+// gui.add(mesh.position, 'y', -3, 3, 0.01);
+// You can also write it by chaining methods like this :
+gui.add(mesh.position, 'y').min(-3).max(3).step(0.01).name('red cube y');
+gui.add(mesh.position, 'x', -3, 3, 0.01);
+gui.add(mesh.position, 'z', -3, 3, 0.01);
+gui.add(mesh, 'visible');
+gui.add(material, 'wireframe');
+gui.addColor(parameters, 'color').onChange(() => {
+	material.color.set(parameters.color);
+});
+gui.add(parameters, 'spin');
+
+/**
+ * Sizes
+ */
 const sizes = {
 	width: window.innerWidth,
 	height: window.innerHeight,
@@ -52,7 +73,10 @@ window.addEventListener('resize', () => {
 	renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 });
 
-// Camera
+/**
+ * Camera
+ */
+// Base camera
 const camera = new THREE.PerspectiveCamera(
 	75,
 	sizes.width / sizes.height,
@@ -66,14 +90,18 @@ scene.add(camera);
 const controls = new OrbitControls(camera, canvas);
 controls.enableDamping = true;
 
-// Renderer
+/**
+ * Renderer
+ */
 const renderer = new THREE.WebGLRenderer({
 	canvas: canvas,
 });
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
-// Animate
+/**
+ * Animate
+ */
 const clock = new THREE.Clock();
 
 const tick = () => {
